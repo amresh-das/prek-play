e91155d2211e4c6a0f7892e63095e7bb3a1e268e
dasa <Amresh.Das@SincroDigital.com>
2022-02-15 14:15:24 +0530

    Rebrand app

--- src/app/evs/clock/clock.component.ts
+++ src/app/evs/clock/clock.component.ts
@@ -14,8 +14,10 @@ export class ClockComponent implements OnInit {
   tickMargin: number = 10;
   clockRadius: number;
   clockCenter: Point;
-  hourRadiusModifier = 0.80;
-  minuteRadiusModifier = 0.92;
+  private readonly hourRadiusModifier = 0.80;
+  private readonly minuteRadiusModifier = 0.92;
+  private readonly majorMarkerTextSizeToRadius = 0.09;
+  private readonly minorMarkerTextSizeToRadius = 0.035;
 
   constructor() {
   }
@@ -34,11 +36,11 @@ export class ClockComponent implements OnInit {
   }
 
   static getSvgSize() {
-    return Math.min(window.innerWidth, window.innerHeight) * .85;
+    return Math.min(window.innerWidth, window.innerHeight) * .75;
   }
 
   static getClockRadius() {
-    return ClockComponent.getSvgSize() * .45;
+    return ClockComponent.getSvgSize() * .40;
   }
 
   getTicks(n: number, incrementBy?: number): number[] {
@@ -62,16 +64,16 @@ export class ClockComponent implements OnInit {
     return "rotate(" + i * 6 + "),translate(-3,-" + (this.clockRadius - this.tickMargin) + ")";
   }
 
-  getX(i: number, total: number): number {
+  getX(i: number, total: number, offsetPercent?: number): number {
     const deg = 270 + ClockComponent.getDeg(i, total);
-    const radius = this.clockRadius * (ClockComponent.isHourMarker(i) ? this.hourRadiusModifier : this.minuteRadiusModifier);
+    const radius = this.clockRadius * ((offsetPercent ? offsetPercent : 0) + (ClockComponent.isHourMarker(i) ? this.hourRadiusModifier : this.minuteRadiusModifier));
     const deviation = 0;
     return deviation + this.clockCenter.x + radius * Math.cos(deg * (Math.PI / 180));
   }
 
-  getY(i: number, total: number): number {
+  getY(i: number, total: number, offsetPercent?: number): number {
     const deg = 270 + ClockComponent.getDeg(i, total);
-    const radius = this.clockRadius * (ClockComponent.isHourMarker(i) ? this.hourRadiusModifier : this.minuteRadiusModifier);
+    const radius = this.clockRadius * ((offsetPercent ? offsetPercent : 0) + (ClockComponent.isHourMarker(i) ? this.hourRadiusModifier : this.minuteRadiusModifier));
     const deviation = 0;
     return deviation + this.clockCenter.y + radius * Math.sin(deg * (Math.PI / 180));
   }
@@ -85,17 +87,38 @@ export class ClockComponent implements OnInit {
   }
 
   getTextSize(tick: number): string {
-    return (ClockComponent.isHourMarker(tick) ? 0.09 : 0.035) * this.clockRadius + 'px';
+    return (ClockComponent.isHourMarker(tick) ? this.majorMarkerTextSizeToRadius : this.minorMarkerTextSizeToRadius) * this.clockRadius + 'px';
   }
 
-  getTextTransform(i: number): string {
+  getTextTransform(i: number, allMinor?: boolean): string {
+    const isMajor = allMinor ? false : ClockComponent.isHourMarker(i);
+    const position: Position = this.getPosition(i);
+    const faceValue = isMajor ? this.getTickHourValue(i) : i;
+
     const modifierFor12Major = 2.16;
     const modifierFor10And11Major = 1.9;
     const modifierFor10To12Minor = 0.8;
     const modifierForAllMinor = 0.25;
     const xDev = this.clockRadius * 0.029 * (!ClockComponent.isHourMarker(i) ? i >= 50 || i < 5 ? modifierFor10To12Minor : modifierForAllMinor : i >= 50 ? modifierFor10And11Major : i < 5 ? modifierFor12Major : 1);
     const yDev = this.clockRadius * 0.024 * (!ClockComponent.isHourMarker(i) ? modifierForAllMinor : 1);
+
+    let xOffset: number, yOffset: number;
     return "translate(-" + xDev + "," + yDev + ")";
+  }
+
+  private getPosition(i: number) {
+    if (i % 15 == 0) {
+      if (i == 0) return Position.TOP;
+      if (i == 15) return Position.RIGHT;
+      if (i == 30) return Position.BOTTOM;
+      return Position.RIGHT;
+    } else {
+      const quadrant = Math.floor(i / 15);
+      if (quadrant == 0) return Position.FIRSTQUADRANT;
+      if (quadrant == 1) return Position.SECONDQUADRANT;
+      if (quadrant == 2) return Position.THIRDQUADRENT;
+      return Position.FOURTHQUADRENT;
+    }
   }
 
   private static isHourMarker(tick: number): boolean {
@@ -145,3 +168,16 @@ export class ClockComponent implements OnInit {
     }
   }
 }
+
+
+enum Position {
+  TOP,
+  BOTTOM,
+  LEFT,
+  RIGHT,
+  FIRSTQUADRANT,
+  SECONDQUADRANT,
+  THIRDQUADRENT,
+  FOURTHQUADRENT
+}
+
