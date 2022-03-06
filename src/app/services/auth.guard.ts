c11a4634b2d1081ff7fc795e22a9e680a7de763c
askds <askdsden@gmail.com>
2022-03-06 23:23:44 +0530

    Add UserController

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,7 +1,7 @@
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
-import {Observable} from "rxjs";
-import {Injectable} from "@angular/core";
-import {SocialAuthService} from "angularx-social-login";
+import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {SocialAuthService} from 'angularx-social-login';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ import {SocialAuthService} from "angularx-social-login";
 export class AuthGuard implements CanActivate {
   constructor(private authService: SocialAuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state:RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return new Observable(observer => {
       this.authService.authState.subscribe(user => observer.next(user !== null));
     });
